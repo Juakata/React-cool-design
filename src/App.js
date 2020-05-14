e3059c93312e3a5a8f030dad8fdc240f6b824fb8
Juakata <juakata07@gmail.com>
2020-05-14 16:57:53 -0600

    finish app file

--- src/App.js
+++ src/App.js
@@ -10,10 +10,21 @@ class App extends React.Component {
     }
   }
 
+  onMouseMove = event => {
+    const val1 = -event.clientX / event.clientY * 2;
+    const val2 = event.clientX / event.clientY * 2;
+    this.setState({
+      styleOne: { transform: `skewY(${val1}deg)` },
+      styleTwo: { transform: `skewY(${val2}deg)` },
+    });
+  }
+
   render() {
+    const { styleOne, styleTwo } = this.state;
     return (
-      <div>
-        Hi
+      <div onMouseMove={this.onMouseMove}>
+        <div className="panel" style={styleOne} />
+        <div className="panel" style={styleTwo} />
       </div>
     );
   }
